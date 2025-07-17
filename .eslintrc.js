fb1d8768c1b44666d0311a33bc040c711283a2e3
Teppo Kurki <teppo.kurki@iki.fi>
2025-07-17 13:10:42 +0300

    chore: add eslint & prettier

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    'no-console': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off'
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+  ignorePatterns: ['dist/', 'node_modules/', '*.js'],
+};
